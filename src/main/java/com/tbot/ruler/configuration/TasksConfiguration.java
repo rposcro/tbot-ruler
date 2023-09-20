3c2de8eb022fa63455722d04a6b523e6cc1ebd23
rposcro <robert.poskrobek@remitly.com>
2023-09-21 00:45:34 +0200

    Introduced tasks service

--- src/main/java/com/tbot/ruler/configuration/TasksConfiguration.java
+++ src/main/java/com/tbot/ruler/configuration/TasksConfiguration.java
@@ -2,11 +2,6 @@ package com.tbot.ruler.configuration;
 
 import com.tbot.ruler.persistance.ApplianceStateRepository;
 import com.tbot.ruler.persistance.json.JsonFileApplianceStateRepository;
-import com.tbot.ruler.service.things.ThingsLifetimeService;
-import com.tbot.ruler.things.TaskBasedItem;
-import com.tbot.ruler.threads.EmissionTriggerContext;
-import com.tbot.ruler.threads.Task;
-import com.tbot.ruler.threads.TaskTrigger;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
@@ -17,17 +12,12 @@ import org.springframework.scheduling.concurrent.ConcurrentTaskExecutor;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 import java.util.Date;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.Executors;
 
 @Slf4j
 @Configuration
 public class TasksConfiguration {
-
-    @Autowired
-    private ThingsLifetimeService thingsLifetimeService;
 
     @Autowired
     private ApplianceStateRepository applianceStateRepository;
@@ -62,44 +52,5 @@ public class TasksConfiguration {
             );
             log.info("Scheduled periodic persistence flush task");
         }
-    }
-
-    @EventListener
-    public void launchTaskBasedItemsThreads(ApplicationReadyEvent event) {
-        List<TaskBasedItem> taskBasedItems = taskBasedItems();
-        taskBasedItems.stream()
-                .flatMap(item -> item.getAsynchronousTasks().stream())
-                .filter(Task::runOnStartUp)
-                .map(Task::getRunnable)
-                .forEach(runnable -> startUpTasksExecutor().execute(runnable));
-
-        taskBasedItems.stream()
-                .flatMap(item -> item.getAsynchronousTasks().stream())
-                .filter(Task::runContinuously)
-                .map(Task::getRunnable)
-                .forEach(runnable -> continuousTasksExecutor().execute(runnable));
-
-        taskBasedItems.stream()
-                .flatMap(item -> item.getAsynchronousTasks().stream())
-                .filter(Task::runOnTrigger)
-                .forEach(task -> {
-                    Runnable runnable = task.getRunnable();
-                    TaskTrigger trigger = task.getTaskTrigger();
-                    triggerableTasksScheduler().schedule(
-                            runnable,
-                            context -> {
-                                Date nextEmissionTime = trigger.nextEmissionTime(new EmissionTriggerContext(context.lastScheduledExecutionTime()));
-                                log.info("Next emission time for {} is {}", task, nextEmissionTime);
-                                return nextEmissionTime;
-                            });
-                });
-    }
-
-    private List<TaskBasedItem> taskBasedItems() {
-        LinkedList<TaskBasedItem> items = new LinkedList<>();
-        items.addAll(thingsLifetimeService.getAllPlugins());
-        items.addAll(thingsLifetimeService.getAllThings());
-        items.addAll(thingsLifetimeService.getAllActuators());
-        return items;
     }
 }
---
+++ src/main/java/com/tbot/ruler/service/things/TasksLifetimeService.java
@@ -0,0 +1,71 @@
+package com.tbot.ruler.service.things;
+
+import com.tbot.ruler.things.TaskBasedItem;
+import com.tbot.ruler.threads.EmissionTriggerContext;
+import com.tbot.ruler.threads.Task;
+import com.tbot.ruler.threads.TaskTrigger;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.scheduling.concurrent.ConcurrentTaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+
+@Slf4j
+@Service
+@Scope("singleton")
+public class TasksLifetimeService {
+
+    @Autowired
+    private ThingsLifetimeService thingsLifetimeService;
+    @Autowired
+    private ThreadPoolTaskScheduler triggerableTasksScheduler;
+    @Autowired
+    private ConcurrentTaskExecutor continuousTasksExecutor;
+    @Autowired
+    private ConcurrentTaskExecutor startUpTasksExecutor;
+
+    @PostConstruct
+    public void init() {
+        List<TaskBasedItem> taskBasedItems = taskBasedItems();
+        taskBasedItems.stream()
+                .flatMap(item -> item.getAsynchronousTasks().stream())
+                .filter(Task::runOnStartUp)
+                .map(Task::getRunnable)
+                .forEach(runnable -> startUpTasksExecutor.execute(runnable));
+
+        taskBasedItems.stream()
+                .flatMap(item -> item.getAsynchronousTasks().stream())
+                .filter(Task::runContinuously)
+                .map(Task::getRunnable)
+                .forEach(runnable -> continuousTasksExecutor.execute(runnable));
+
+        taskBasedItems.stream()
+                .flatMap(item -> item.getAsynchronousTasks().stream())
+                .filter(Task::runOnTrigger)
+                .forEach(task -> {
+                    Runnable runnable = task.getRunnable();
+                    TaskTrigger trigger = task.getTaskTrigger();
+                    triggerableTasksScheduler.schedule(
+                            runnable,
+                            context -> {
+                                Date nextEmissionTime = trigger.nextEmissionTime(new EmissionTriggerContext(context.lastScheduledExecutionTime()));
+                                log.info("Next emission time for {} is {}", task, nextEmissionTime);
+                                return nextEmissionTime;
+                            });
+                });
+    }
+
+    private List<TaskBasedItem> taskBasedItems() {
+        LinkedList<TaskBasedItem> items = new LinkedList<>();
+        items.addAll(thingsLifetimeService.getAllPlugins());
+        items.addAll(thingsLifetimeService.getAllThings());
+        items.addAll(thingsLifetimeService.getAllActuators());
+        return items;
+    }
+}
